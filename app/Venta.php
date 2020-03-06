acad42642b063ce0836f0c5af168f87e6d3c9b84
Luis Cabrera Benito <parzibyte@gmail.com>
2020-03-06 02:18:45 -0600

    Créditos y quitando welcome

--- app/Venta.php
+++ app/Venta.php
@@ -1,4 +1,25 @@
 <?php
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+    Blog:       https://parzibyte.me/blog
+    Ayuda:      https://parzibyte.me/blog/contrataciones-ayuda/
+    Contacto:   https://parzibyte.me/blog/contacto/
+    
+    Copyright (c) 2020 Luis Cabrera Benito
+    Licenciado bajo la licencia MIT
+    
+    El texto de arriba debe ser incluido en cualquier redistribucion
+*/ ?>
+<?php
 
 namespace App;
 
