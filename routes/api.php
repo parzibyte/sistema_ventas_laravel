c96d296eda5c38f48c22b96fb2e1d83f1af84d7b
Luis Cabrera Benito <parzibyte@gmail.com>
2020-03-23 01:42:08 -0600

    API para CRUD productos
    La API ahora soporta los 4 métodos para los productos. Bueno, son 5 porque hay 2 get, pero uno de ellos sirve para obtener el producto y rellenar el formulario para editar

--- routes/api.php
+++ routes/api.php
@@ -32,12 +32,31 @@ Route::group(['prefix' => 'auth'], function () {
         Route::get("productos", function () {
             return response()->json(\App\Producto::all());
         });
+        /*
+            Si existe un dios, que me perdone por dejar todas las peticiones aquí
+            en lugar de separarlas a otro archivo o invocar un controlador
+        */
         Route::post("/producto", function(Request $request){
             // return $request->input();
             $producto = new Producto($request->input());
             // return $producto;
             $producto->saveOrFail();
             return response()->json(["data" => "true"]);
+        });
+        Route::get("/producto/{id}", function($id){
+            $producto = Producto::findOrFail($id);
+            return response()->json($producto);
+        });
+        Route::put("/producto", function(Request $request){
+            $producto = Producto::findOrFail($request->input("id"));
+            $producto->fill($request->input());
+            $producto->saveOrFail();
+            return response()->json(true);
+        });
+        Route::delete("/producto/{id}", function($id){
+            $producto = Producto::findOrFail($id);
+            $producto->delete();
+            return response()->json(true);
         });
     });
 });
